9b69bef9ba4dd7d9b9172ee19d1004c531f88736
Neil R. Zamora <neil.z@devandgo.com>
2023-08-17 00:00:01 +0800

    Resolve several issues and add feature enhancements
    - Enhance README
    
    - Fix callback_server.py
      * Mistakenly used HTTP_RID header instead of `rid`.
      * Added Orignal-Status and PC-Status checking and properly parse it
        because it has a value like "PC-Status: 200, 200".
      * Unzip body only if Content-Encoding header is gzip.
    
    - Fix process.py
      * The code was not properly indented when it was delivered in the logic was affected.
      * Create a database session everytime it process as opposed to creating it one time.
    
    - Added database index definitions
    
    - Renamed linkedin_profile_urls.txt to more simpler urls.txt
    
    - Added .gitignore

--- callback_server.py
+++ callback_server.py
@@ -11,36 +11,53 @@ session = create_database_session()
 
 @app.route('/crawlbase_crawler_callback', methods=['POST'])
 def crawlbase_crawler_callback():
-  crawlbase_rid = request.headers.get('HTTP_RID')
+  crawlbase_rid = request.headers.get('rid')
+  content_encoding = request.headers.get('Content-Encoding')
+  original_status = request.headers.get('Original-Status')
+  if (not original_status is None):
+    original_status = int(original_status.split(',')[0])
+  crawlbase_status = request.headers.get('PC-Status')
+  if (not crawlbase_status is None):
+    crawlbase_status = int(crawlbase_status.split(',')[0])
 
   if crawlbase_rid is None:
     log(f'Crawlbase rid is not set.')
     return ('', 204)
 
-  if crawlbase_rid == 'test':
+  if crawlbase_rid == 'dummyrequest':
     log('Callback server is working')
     return ('', 204)
 
-  crawl_request_does_not_exist = session.query(CrawlRequest).filter_by(crawlbase_rid=crawlbase_rid).first() is None
+  if crawlbase_status != 200:
+    log(f'Crawlbase status is not 200.')
+    return ('', 204)
+
+  if original_status != 200:
+    log(f'Original status is not 200.')
+    return ('', 204)
+
+  crawl_request_does_not_exist = session.query(CrawlRequest).filter_by(crawlbase_rid=crawlbase_rid, status='waiting').first() is None
 
   if crawl_request_does_not_exist:
-    log(f'Crawlbase rid {crawlbase_rid} does not exist in the database.')
+    log(f'No Crawlbase rid {crawlbase_rid} found with status `waiting`.')
     return ('', 204)
 
-  crawl_request = session.query(CrawlRequest).filter_by(crawlbase_rid = crawlbase_rid).scalar()
+  crawl_request = session.query(CrawlRequest).filter_by(crawlbase_rid=crawlbase_rid, status='waiting').scalar()
 
-  if crawl_request.status != 'waiting':
-    log(f'Crawlbase rid {crawlbase_rid} might have been already processed.')
-    return ('', 204)
+  body = request.data
+  if content_encoding == 'gzip':
+    try:
+      body = gzip.decompress(body)
+    except OSError:
+      pass
 
-  data = gzip.decompress(request.data)
   with open(f'./data/{crawlbase_rid}.json', 'wb') as f:
-    f.write(data)
+    f.write(body)
   
   crawl_request.status = 'received'
   session.commit()
 
-  log(f'Crawlbase rid {crawlbase_rid} was successfully processed.')
+  log(f'Crawlbase rid {crawlbase_rid} was successfully received.')
 
   return ('', 201)
 
